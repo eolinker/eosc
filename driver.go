8bae7483266c64dfa04a67f71e81aa2771be5a37
Liujian <824010343@qq.com>
2021-07-28 10:37:32 +0800

    新增IProfessionDriverConfigCheck接口

--- driver.go
+++ driver.go
@@ -26,6 +26,10 @@ type IProfessionDriverFactory interface {
 	Create(profession string, name string, label string, desc string, params map[string]string) (IProfessionDriver, error)
 }
 
+type IProfessionDriverConfigCheck interface {
+	Check(id, name string, v interface{}, workers map[RequireId]interface{}) error
+}
+
 type IProfessionDriver interface {
 	ConfigType() reflect.Type
 	Create(id, name string, v interface{}, workers map[RequireId]interface{}) (IWorker, error)
