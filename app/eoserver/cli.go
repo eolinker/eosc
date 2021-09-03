aea809b4994e0b9d7a916da19927709db55ad412
Liujian <824010343@qq.com>
2021-09-03 18:39:18 +0800

    master加上error日志文件

--- app/eoserver/cli.go
+++ app/eoserver/cli.go
@@ -3,9 +3,13 @@ package main
 import (
 	"errors"
 	"fmt"
+	"io/ioutil"
 	"net/url"
 	"os"
+	"strconv"
 	"strings"
+
+	eosc_args "github.com/eolinker/eosc/eosc-args"
 
 	"github.com/eolinker/eosc/process"
 
@@ -23,6 +27,8 @@ func start(c *cli.Context) error {
 	if err != nil {
 		return err
 	}
+	os.Setenv(fmt.Sprintf("%s_%s", process.AppName(), eosc_args.IP), ip)
+	os.Setenv(fmt.Sprintf("%s_%s", process.AppName(), eosc_args.Port), strconv.Itoa(port))
 	args = append(args, "start", fmt.Sprintf("--ip=%s", ip), fmt.Sprintf("--port=%d", port))
 	join := c.Bool("join")
 	if join {
@@ -45,6 +51,7 @@ func start(c *cli.Context) error {
 			validAddr = true
 			args = append(args, fmt.Sprintf("--cluster-addr=%s", a))
 		}
+		os.ExpandEnv()
 		if !validAddr {
 			return errors.New("no valid cluster address")
 		}
@@ -84,7 +91,22 @@ func clusters(c *cli.Context) error {
 	return nil
 }
 
-func writeConfig(params map[string]string) {
+func writeConfig(params map[string]string) error {
+	err := os.MkdirAll("work/", 0700)
+	if err != nil {
+		return err
+	}
+	builder := strings.Builder{}
+	for key, value := range params {
+		builder.WriteString(key)
+		builder.WriteString("=")
+		builder.WriteString(value)
+		builder.WriteString("\n")
+	}
+	return ioutil.WriteFile(fmt.Sprintf("work/%s.args"), []byte(builder.String()))
+}
+
+func readConfig() (map[string]string, error) {
 
 }
 
