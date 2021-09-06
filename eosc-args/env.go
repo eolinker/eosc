b6fb3e77fe5f4c982166bee7601ed6291e9e5c7f
黄孟柱 <hmzzrcs@163.com>
2021-09-06 10:43:44 +0800

    提交

--- eosc-args/env.go
+++ eosc-args/env.go
@@ -1,7 +1,24 @@
 package eosc_args
 
+import (
+	"fmt"
+	"github.com/eolinker/eosc/process"
+	"syscall"
+)
+
 const IP = "IP"
 const Port = "PORT"
 const BroadcastIP = "BROADCAST_IP"
 const BroadcastPort = "BROADCAST_PORT"
 const ClusterAddress = "ClusterAddress"
+const PluginPath = "Plugins_DIR"
+
+func GetEnv(name string) (string, bool) {
+	return syscall.Getenv(envName(name))
+}
+func SetEnv(name string, value string)error  {
+	return syscall.Setenv(envName(name),value)
+}
+func envName(name string)string  {
+	return fmt.Sprintf("%s_%s", process.AppName(), name)
+}
