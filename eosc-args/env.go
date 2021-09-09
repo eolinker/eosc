c12143f1a79544e2c0182d0b7d654a29996d8b86
黄孟柱 <hmzzrcs@163.com>
2021-09-09 11:11:42 +0800

    app name 改为 eosc_args

--- eosc-args/env.go
+++ eosc-args/env.go
@@ -2,9 +2,9 @@ package eosc_args
 
 import (
 	"fmt"
+	"os"
+	"path/filepath"
 	"syscall"
-
-	"github.com/eolinker/eosc/process"
 )
 
 const IP = "IP"
@@ -17,13 +17,26 @@ const PluginPath = "PLUGINS_DIR"
 var envs = []string{
 	IP, Port, BroadcastIP, BroadcastPort, ClusterAddress, PluginPath,
 }
+var (
+	appName = createApp()
+)
 
+func createApp() string {
+	if app, has := os.LookupEnv("APP"); has {
+		return app
+	}
+	app := filepath.Base(os.Args[0])
+	if err := os.Setenv("APP", app); err != nil {
+		panic(err)
+	}
+	return app
+}
 func Envs() []string {
 	return envs
 }
 
 func GetEnv(name string) (string, bool) {
-	return syscall.Getenv(envName(name))
+	return syscall.Getenv(EnvName(name))
 }
 
 func GetDefault(name string, d string) string {
@@ -33,11 +46,15 @@ func GetDefault(name string, d string) string {
 	return d
 }
 func SetEnv(name, value string) error {
-	return syscall.Setenv(envName(name), value)
+	return syscall.Setenv(EnvName(name), value)
 }
 func GenEnv(name, value string) string {
-	return fmt.Sprintf("%s=%s", envName(name), value)
+	return fmt.Sprintf("%s=%s", EnvName(name), value)
 }
-func envName(name string) string {
-	return fmt.Sprintf("%s_%s", process.AppName(), name)
+func EnvName(name string) string {
+	return fmt.Sprintf("%s_%s", appName, name)
+}
+
+func AppName() string {
+	return appName
 }
