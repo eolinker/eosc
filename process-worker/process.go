313244f088889364252462d4e1678edce73f156f
黄孟柱 <hmzzrcs@163.com>
2021-10-11 10:42:09 +0800

    优化worker process 的rpc 的启动

--- process-worker/process.go
+++ process-worker/process.go
@@ -14,18 +14,12 @@ import (
 	"sync"
 	"syscall"
 
-	"github.com/eolinker/eosc/env"
 	"github.com/eolinker/eosc/utils"
 
 	"github.com/eolinker/eosc"
 	"github.com/eolinker/eosc/common/bean"
 
-	grpc_unixsocket "github.com/eolinker/eosc/grpc-unixsocket"
-	"github.com/eolinker/eosc/service"
-	"google.golang.org/grpc"
-
 	"github.com/eolinker/eosc/log"
-
 	"github.com/eolinker/eosc/traffic"
 )
 
@@ -52,8 +46,9 @@ type ProcessWorker struct {
 	tf          traffic.ITraffic
 	professions IProfessions
 	workers     IWorkers
-	srv         *grpc.Server
-	once        sync.Once
+
+	once         sync.Once
+	workerServer *WorkerServer
 }
 
 func (w *ProcessWorker) wait() {
@@ -87,7 +82,14 @@ func (w *ProcessWorker) wait() {
 //NewProcessWorker 创建新的worker进程
 //启动时通过stdin传输配置信息
 func NewProcessWorker() (*ProcessWorker, error) {
-	w := &ProcessWorker{}
+	workerServer, err := NewWorkerServer()
+	if err != nil {
+		return nil, err
+	}
+	w := &ProcessWorker{
+		workerServer: workerServer,
+	}
+
 	tf := traffic.NewTraffic()
 	w.tf = tf
 	ps := NewProfessions()
@@ -124,33 +126,15 @@ func (w *ProcessWorker) close() {
 
 	w.once.Do(func() {
 		w.tf.Close()
-		w.srv.Stop()
+		w.workerServer.Stop()
 
-		addr := service.WorkerServerAddr(env.AppName(), os.Getpid())
-		// 移除unix socket
-		syscall.Unlink(addr)
 	})
 
 }
 
 func (w *ProcessWorker) Start() error {
-	addr := service.WorkerServerAddr(env.AppName(), os.Getpid())
-	// 移除unix socket
-	syscall.Unlink(addr)
 
-	log.Info("start worker :", addr)
-	l, err := grpc_unixsocket.Listener(addr)
-	if err != nil {
-		return err
-	}
+	w.workerServer.SetWorkers(w.workers)
 
-	grpcServer := grpc.NewServer()
-
-	service.RegisterWorkerServiceServer(grpcServer, NewWorkerServer(w.workers))
-	go func() {
-		grpcServer.Serve(l)
-	}()
-
-	w.srv = grpcServer
 	return nil
 }
--- process-worker/service.go
+++ process-worker/service.go
@@ -2,6 +2,13 @@ package process_worker
 
 import (
 	"context"
+	"os"
+	"syscall"
+
+	"github.com/eolinker/eosc/env"
+	grpc_unixsocket "github.com/eolinker/eosc/grpc-unixsocket"
+
+	"google.golang.org/grpc"
 
 	"github.com/eolinker/eosc/log"
 	"github.com/eolinker/eosc/service"
@@ -9,16 +16,51 @@ import (
 
 type WorkerServer struct {
 	service.UnimplementedWorkerServiceServer
-
+	*grpc.Server
 	workers IWorkers
 }
 
-func NewWorkerServer(workers IWorkers) *WorkerServer {
-	return &WorkerServer{workers: workers}
+func (ws *WorkerServer) SetWorkers(workers IWorkers) {
+	ws.workers = workers
+}
+func (ws *WorkerServer) Stop() {
+	ws.Server.Stop()
+	addr := service.WorkerServerAddr(env.AppName(), os.Getpid())
+	// 移除unix socket
+	syscall.Unlink(addr)
+}
+func NewWorkerServer() (*WorkerServer, error) {
+
+	addr := service.WorkerServerAddr(env.AppName(), os.Getpid())
+	// 移除unix socket
+	syscall.Unlink(addr)
+	log.Info("start worker :", addr)
+	l, err := grpc_unixsocket.Listener(addr)
+	if err != nil {
+		return nil, err
+	}
+	ws := &WorkerServer{workers: nil, Server: grpc.NewServer()}
+	service.RegisterWorkerServiceServer(ws.Server, ws)
+	go func() {
+		err := ws.Server.Serve(l)
+		if err != nil {
+			log.Info("grpc server:", err)
+			return
+		}
+	}()
+	return ws, nil
+
 }
 
 func (ws *WorkerServer) DeleteCheck(ctx context.Context, request *service.WorkerDeleteRequest) (*service.WorkerDeleteResponse, error) {
 	log.Debug("delete check: ", request.Id)
+	if ws.workers == nil {
+		return &service.WorkerDeleteResponse{
+			Status:   service.WorkerStatusCode_FAIL,
+			Message:  "Initializing",
+			Resource: nil,
+		}, nil
+	}
 	count := ws.workers.RequiredCount(request.Id)
 	if count > 0 {
 		return &service.WorkerDeleteResponse{
@@ -40,7 +82,13 @@ func (ws *WorkerServer) DeleteCheck(ctx context.Context, request *service.Worker
 func (ws *WorkerServer) SetCheck(ctx context.Context, req *service.WorkerSetRequest) (*service.WorkerSetResponse, error) {
 	log.Debug("set check: ", req.Id, " ", req.Profession, " ", req.Name, " ", req.Driver, " ", string(req.Body))
 	err := ws.workers.Check(req.Id, req.Profession, req.Name, req.Driver, req.Body)
-
+	if ws.workers == nil {
+		return &service.WorkerSetResponse{
+			Status:   service.WorkerStatusCode_FAIL,
+			Message:  "Initializing",
+			Resource: nil,
+		}, nil
+	}
 	if err != nil {
 		log.Info("serivce set :", err)
 		return &service.WorkerSetResponse{
@@ -62,7 +110,15 @@ func (ws *WorkerServer) SetCheck(ctx context.Context, req *service.WorkerSetRequ
 
 func (ws *WorkerServer) Delete(ctx context.Context, request *service.WorkerDeleteRequest) (*service.WorkerDeleteResponse, error) {
 	log.Debug("delete: ", request.Id)
+	if ws.workers == nil {
+		return &service.WorkerDeleteResponse{
+			Status:   service.WorkerStatusCode_FAIL,
+			Message:  "Initializing",
+			Resource: nil,
+		}, nil
+	}
 	err := ws.workers.Del(request.Id)
+
 	if err != nil {
 		log.Info("delete fail:", err)
 		return &service.WorkerDeleteResponse{
@@ -84,6 +140,13 @@ func (ws *WorkerServer) Delete(ctx context.Context, request *service.WorkerDelet
 
 func (ws *WorkerServer) Set(ctx context.Context, req *service.WorkerSetRequest) (*service.WorkerSetResponse, error) {
 	log.Debug("set: ", req.Id, " ", req.Profession, " ", req.Name, " ", req.Driver, " ", string(req.Body))
+	if ws.workers == nil {
+		return &service.WorkerSetResponse{
+			Status:   service.WorkerStatusCode_FAIL,
+			Message:  "Initializing",
+			Resource: nil,
+		}, nil
+	}
 	err := ws.workers.Set(req.Id, req.Profession, req.Name, req.Driver, req.Body)
 	if err != nil {
 		log.Info("worker server set:", err)
@@ -105,6 +168,13 @@ func (ws *WorkerServer) Set(ctx context.Context, req *service.WorkerSetRequest) 
 }
 
 func (ws *WorkerServer) Ping(ctx context.Context, request *service.WorkerHelloRequest) (*service.WorkerHelloResponse, error) {
+	if ws.workers == nil {
+		return &service.WorkerHelloResponse{
+			Resource: &service.WorkerResource{
+				Port: []int32{},
+			},
+		}, nil
+	}
 	return &service.WorkerHelloResponse{
 		Hello: request.Hello,
 		Resource: &service.WorkerResource{
