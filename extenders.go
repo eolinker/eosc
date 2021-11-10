0a465e6bf3f7c9f5f3584c9c33ec20bb4779291b
黄孟柱 <hmzzrcs@163.com>
2021-11-10 16:44:05 +0800

    插件管理器对接驱动

--- extenders.go
+++
@@ -1,8 +0,0 @@
-package eosc
-
-type IExtendersSetting interface {
-	SetExtender(group, project, version string) error
-	DelExtender(group, project string) (string, bool)
-	GetExtenderVersion(group, project string) (string, bool)
-	//ALl()map[string]string
-}
