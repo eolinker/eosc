2ea4080a885f2fac548a6ca4c98c8094aebf3964
黄孟柱 <hmzzrcs@163.com>
2021-12-14 11:31:05 +0800

    增加clear error 操作

--- http-service/context.go
+++ http-service/context.go
@@ -16,7 +16,6 @@ type IHttpContext interface {
 	Request() IRequestReader // 读取原始请求
 	Proxy() IRequest         // 读写转发请求
 	Response() IResponse     // 处理返回结果，可读可写
-	ResponseError() error
 	SendTo(address string, timeout time.Duration) error
 }
 
@@ -143,6 +142,8 @@ type IRequest interface {
 
 // 返回给client端的
 type IResponse interface {
+	ResponseError() error
+	ClearError()
 	IStatusGet
 	IResponseHeader
 	IStatusSet // 设置返回状态
