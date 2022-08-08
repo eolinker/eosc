fc6cc018703fe8c797febce9912d1ba183a32d67
Liujian <824010343@qq.com>
2022-08-08 13:33:48 +0800

    变量管理器完成

--- variable/manager.go
+++ variable/manager.go
@@ -1,46 +1,81 @@
 package variable
 
 import (
+	"fmt"
 	"github.com/eolinker/eosc"
 	"github.com/eolinker/eosc/log"
-	"github.com/eolinker/eosc/workers/require"
-	"sync"
 )
 
 type IVariable interface {
-	SetByNamespace(namespace string, variables map[string]string) (map[string]string, []string)
+	SetByNamespace(namespace string, variables map[string]string) (map[string]string, []string, error)
 	GetByNamespace(namespace string) (map[string]string, bool)
-	Get() map[string]string
+	SetVariablesById(id string, variables []string)
+	GetVariablesById(id string) []string
+	GetIdsByVariable(variable string) []string
+	GetAll() map[string]string
 }
 
 type Manager struct {
 	// variables 变量数据
 	variables      eosc.IUntyped
-	requireManager require.IRequires
-	locker         sync.RWMutex
+	requireManager IRequires
 }
 
-func (m *Manager) SetByNamespace(namespace string, variables map[string]string) (map[string]string, []string) {
+func (m *Manager) SetVariablesById(id string, variables []string) {
+	m.requireManager.Set(id, variables)
+}
+
+func (m *Manager) GetVariablesById(id string) []string {
+	return m.requireManager.WorkerIDs(id)
+}
+
+func (m *Manager) GetIdsByVariable(variable string) []string {
+	return m.requireManager.RequireIDs(variable)
+}
+
+func (m *Manager) SetByNamespace(namespace string, variables map[string]string) (map[string]string, []string, error) {
 	// variables的key为：{变量名}@{namespace}，如：v1@default
 	old, has := m.getByNamespace(namespace)
 	if !has {
-		// 此时变量都是新的，没有受影响的配置id
 		m.variables.Set(namespace, variables)
-		newVariables := make(map[string]string)
-		for _, key := range m.variables.Keys() {
-			vs, ok := m.getByNamespace(key)
-			if !ok {
-				log.Error("fail to get variable,namespace is ", namespace)
-				continue
-			}
-			for k, v := range vs {
-				newVariables[k] = v
-			}
-		}
-		return newVariables, nil
+		// 此时变量都是新的，没有受影响的配置id
+		return m.getAll(), nil, nil
 	}
+	affectIds := make([]string, 0, len(variables))
+	for key, value := range variables {
+		if v, ok := old[key]; ok {
+			if v != value {
+				// 将更新的key记录下来
+				affectIds = append(affectIds, m.requireManager.RequireIDs(key)...)
+			}
+			delete(old, key)
+			continue
+		}
+		// 将新增的key记录下来
+		affectIds = append(affectIds, m.requireManager.RequireIDs(key)...)
+	}
+	for key := range old {
+		if m.requireManager.RequireByCount(key) > 0 {
+			return nil, nil, fmt.Errorf("variable %s %w", key, eosc.ErrorRequire)
+		}
+	}
+	m.variables.Set(namespace, variables)
+	return m.getAll(), affectIds, nil
+}
 
-	return newVariables, keys
+func (m *Manager) getAll() map[string]string {
+	newVariables := make(map[string]string)
+	for _, key := range m.variables.Keys() {
+		vs, ok := m.getByNamespace(key)
+		if !ok {
+			log.Error("fail to get variable,namespace is ", key)
+			continue
+		}
+		for k, v := range vs {
+			newVariables[k] = v
+		}
+	}
+	return newVariables
 }
 
 func (m *Manager) getByNamespace(namespace string) (map[string]string, bool) {
@@ -56,11 +91,9 @@ func (m *Manager) getByNamespace(namespace string) (map[string]string, bool) {
 }
 
 func (m *Manager) GetByNamespace(namespace string) (map[string]string, bool) {
-	//TODO implement me
-	panic("implement me")
+	return m.getByNamespace(namespace)
 }
 
-func (m *Manager) Get() map[string]string {
-	//TODO implement me
-	panic("implement me")
+func (m *Manager) GetAll() map[string]string {
+	return m.getAll()
 }
