b67626482408e30d333211562bae50b65bb9ee6d
hmzzrcs <hmzzrcs@163.com>
2024-04-18 10:12:57 +0800

    bugfix

--- utils/math.go
+++ utils/math.go
@@ -1,7 +1,7 @@
 package utils
 
 type Number interface {
-	int | int8 | int16 | int32 | int64 | uint | uint8 | uint16 | uint32 | uint64 | float32 | float64 | byte
+	int | int8 | int16 | int32 | int64 | uint | uint8 | uint16 | uint32 | uint64 | float32 | float64
 }
 
 func Max[T Number](a, b T) T {
