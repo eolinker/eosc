899f64ea9fe707f14879f61bf77aa06259fee116
hmzzrcs <hmzzrcs@163.com>
2024-05-11 11:03:21 +0800

    优化启用 pprof的代码顺序, 使用快速跳过

--- debug/pprof.go
+++ debug/pprof.go
@@ -2,57 +2,47 @@ package debug
 
 import (
 	"fmt"
+	"github.com/eolinker/eosc/env"
+	"github.com/eolinker/eosc/log"
 	"net"
 	"net/http"
 	"net/http/pprof"
-
-	"github.com/eolinker/eosc/env"
-	"github.com/eolinker/eosc/log"
 )
 
-var (
-	mux = http.NewServeMux()
-)
+var ()
 
-func Register(path string, handleFunc func(w http.ResponseWriter, r *http.Request)) {
-	if mux != nil {
-		mux.HandleFunc(path, handleFunc)
-	}
-}
 func RunDebug(name string) {
 
+	addr, has := env.GetEnv(fmt.Sprintf("PPROF_%s", name))
+	log.Debug("pprof addr:", addr, " ", name, ": ", fmt.Sprintf("PPROF_%s", name))
+	if !has || addr == "" {
+		return
+	}
+	listen, err := net.Listen("tcp", addr)
+	if err != nil {
+		log.Warn("fail to listen pprof:", addr)
+
+		return
+	}
+	mux := http.NewServeMux()
 	mux.HandleFunc("/debug/pprof/", pprof.Index)
 	mux.HandleFunc("/debug/pprof/cmdline", pprof.Cmdline)
 	mux.HandleFunc("/debug/pprof/profile", pprof.Profile)
 	mux.HandleFunc("/debug/pprof/symbol", pprof.Symbol)
 	mux.HandleFunc("/debug/pprof/trace", pprof.Trace)
 
-	addr, has := env.GetEnv(fmt.Sprintf("PPROF_%s", name))
-	log.Debug("pprof addr:", addr, " ", name, ": ", fmt.Sprintf("PPROF_%s", name))
-	if has {
+	lAddr := listen.Addr().(*net.TCPAddr)
 
-		listen, err := net.Listen("tcp", addr)
-		if err != nil {
-			log.Warn("fail to listen pprof:", addr)
-			panic(err)
-			return
-		}
-		lAddr := listen.Addr().(*net.TCPAddr)
+	log.Infof("start pprof:\thttp%s:%d", lAddr.IP.String(), lAddr.Port)
 
-		log.Infof("start pprof:\thttp%s:%d", lAddr.IP.String(), lAddr.Port)
-
+	go func() {
 		server := http.Server{
 			Handler: mux,
 		}
-		go func() {
-			err := server.Serve(listen)
-			if err != nil {
-				return
-			}
-		}()
-
-	} else {
-		mux = nil
-	}
+		err := server.Serve(listen)
+		if err != nil {
+			return
+		}
+	}()
 
 }
